5a29f438d32dbab7339265a07062b46d12b1b29c
Vitaly Guzun <vitaliy@miro.com>
2026-04-14 20:23:51 +0200

    Fix client script

--- miro/client.sh
+++ miro/client.sh
@@ -4,7 +4,7 @@
 switch-dev() { serverName="${1:-released}"; cd e2e-tests/client && yarn spectator switch -n dev-custom -s "$serverName" && cd ../../; }
 switch-local() { serverName="${1:-released}"; cd e2e-tests/client && yarn spectator switch -n local-custom -s "$serverName" && cd ../../; }
 switch-env() { envNumber="${1:-12}";  client="${2:-local}"; cd e2e-tests/client && yarn spectator switch -n autotests-"${envNumber}" -c "$client" && cd ../..; }
-switch-which() { cat e2e-tests/client/spectator/.local_spectator }
+switch-which() { cat e2e-tests/client/spectator/.local_spectator; }
 vtest-docker() { allureId="${1}"; repeat="${2:-0}"; cd e2e-tests/client && yarn spectator test --docker --allure-id "${allureId}" -f "${repeat}" && cd ../..; }
 vtest() { allureId="${1}"; repeat="${2:-0}"; cd e2e-tests/client && yarn spectator test --allure-id "${allureId}" -f "${repeat}" && cd ../..; }
 
