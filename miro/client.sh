d689e2cb8ddfd0df5d10da65d38a41bf73d68f71
Vitaly Guzun <vitaliy@miro.com>
2026-04-15 22:31:41 +0200

    Fix client.sh

--- miro/client.sh
+++ miro/client.sh
@@ -1,14 +1,48 @@
 ###############
 ## Client tests
 ###############
-switch-dev() { serverName="${1:-released}"; cd e2e-tests/client && yarn spectator switch -n dev-custom -s "$serverName" && cd ../../; }
-switch-local() { serverName="${1:-released}"; cd e2e-tests/client && yarn spectator switch -n local-custom -s "$serverName" && cd ../../; }
-switch-env() { envName="${1:autotests-12}";  client="${2:-local}"; cd e2e-tests/client && yarn spectator switch -n "${envName}" -c "$client" && cd ../..; }
-switch-which() { cat e2e-tests/client/spectator/.local_spectator; }
-vtest-docker() { allureId="${1}"; repeat="${2:-0}"; cd e2e-tests/client && yarn spectator test --docker --allure-id "${allureId}" -f "${repeat}"; cd ../..; }
-vtest() { allureId="${1}"; repeat="${2:-0}"; cd e2e-tests/client && yarn spectator test --allure-id "${allureId}" -f "${repeat}"; cd ../..; }
+
+switch-dev() {
+  local serverName="${1:-released}"
+  cd e2e-tests/client && yarn spectator switch -n dev-custom -s "$serverName"
+  cd ../../
+}
+
+switch-local() {
+  local serverName="${1:-released}"
+  cd e2e-tests/client && yarn spectator switch -n local-custom -s "$serverName"
+  cd ../../
+}
+
+switch-env() {
+  local envName="${1:-autotests-12}"
+  local client="${2:-local}"
+  cd e2e-tests/client && yarn spectator switch -n "${envName}" -c "$client"
+  cd ../..
+}
+
+switch-which() {
+  cat e2e-tests/client/spectator/.local_spectator
+}
+
+vtest-docker() {
+  local allureId="${1}"
+  local repeat="${2:-0}"
+  cd e2e-tests/client && yarn spectator test --docker --allure-id "${allureId}" -f "${repeat}"
+  cd ../..
+}
+
+vtest() {
+  local allureId="${1}"
+  local repeat="${2:-0}"
+  cd e2e-tests/client && yarn spectator test --allure-id "${allureId}" -f "${repeat}"
+  cd ../..
+}
 
 ###############
 ## Client
 ###############
-vstart() { npx nx reset && nvm use && yarn start; }
+
+vstart() {
+  npx nx reset && nvm use && yarn start
+}
