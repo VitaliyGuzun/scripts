0ea2d2f5e83369adbe5bdbc3302f09635569f254
Vitaly Guzun <vitaliy@miro.com>
2026-04-15 22:25:38 +0200

    Fix client.sh

--- miro/client.sh
+++ miro/client.sh
@@ -3,7 +3,7 @@
 ###############
 switch-dev() { serverName="${1:-released}"; cd e2e-tests/client && yarn spectator switch -n dev-custom -s "$serverName" && cd ../../; }
 switch-local() { serverName="${1:-released}"; cd e2e-tests/client && yarn spectator switch -n local-custom -s "$serverName" && cd ../../; }
-switch-env() { envNumber="${1:-12}";  client="${2:-local}"; cd e2e-tests/client && yarn spectator switch -n autotests-"${envNumber}" -c "$client" && cd ../..; }
+switch-env() { envName="${1:autotests-12}";  client="${2:-local}"; cd e2e-tests/client && yarn spectator switch -n "${envName}" -c "$client" && cd ../..; }
 switch-which() { cat e2e-tests/client/spectator/.local_spectator; }
 vtest-docker() { allureId="${1}"; repeat="${2:-0}"; cd e2e-tests/client && yarn spectator test --docker --allure-id "${allureId}" -f "${repeat}" && cd ../..; }
 vtest() { allureId="${1}"; repeat="${2:-0}"; cd e2e-tests/client && yarn spectator test --allure-id "${allureId}" -f "${repeat}" && cd ../..; }
@@ -11,4 +11,4 @@ vtest() { allureId="${1}"; repeat="${2:-0}"; cd e2e-tests/client && yarn spectat
 ###############
 ## Client
 ###############
-vstart() { npx nx reset && nvm use && yarn start }
+vstart() { npx nx reset && nvm use && yarn start; }
