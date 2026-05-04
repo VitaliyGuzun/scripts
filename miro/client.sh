2747013c014a25d268b66d5ba0a2ccbc90369f97
Vitaly Guzun <vitaliy@miro.com>
2026-05-04 12:32:30 +0200

    Refactor

--- miro/client.sh
+++ miro/client.sh
@@ -2,17 +2,20 @@
 ## Client tests
 ###############
 
-switch-dev() {
-  local serverName="${1:-released}"
-  cd e2e-tests/client && yarn spectator switch -n dev-custom -s "$serverName"
-  cd ../../
-}
+# Examples:
+# switch-local released (default)
+# switch-local lastmaster
+# switch-local your-custom-server-name from https://devconsole.develop.testmiro.com/
 
 switch-local() {
   local serverName="${1:-released}"
   cd e2e-tests/client && yarn spectator switch -n local-custom -s "$serverName"
   cd ../../
 }
+
+# Examples:
+# switch-env autotests-12 local (default)
+# switch-env staging-eu02 your-custom-client-version from CI/CD (1.94983.0 for example)
 
 switch-env() {
   local envName="${1:-autotests-12}"
@@ -21,10 +24,12 @@ switch-env() {
   cd ../..
 }
 
+# Just print your current spectator settings
 switch-which() {
   cat e2e-tests/client/spectator/.local_spectator
 }
 
+# Tests with docker - this checks screenshots too
 vtest-docker() {
   local allureId="${1}"
   local repeat="${2:-0}"
@@ -32,6 +37,7 @@ vtest-docker() {
   cd ../..
 }
 
+# Tests without docker - this does not check screenshots
 vtest() {
   local allureId="${1}"
   local repeat="${2:-0}"
